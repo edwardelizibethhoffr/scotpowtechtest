1d5aa5d60b2fd435c4668843a5cba30696ef6aef
Calum Maclellan <calummaclellan1@gmail.com>
2025-05-07 20:59:38 +0100

    improved detail layout on regular size class

--- ScotPowTechTest/ScotPowTechTest/Presentation/Views/TrackDetailView.swift
+++ ScotPowTechTest/ScotPowTechTest/Presentation/Views/TrackDetailView.swift
@@ -8,33 +8,43 @@
 import SwiftUI
 
 struct TrackDetailView: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
     
     var viewModel: TrackDetailViewModel
     
+    private let imageWidth: CGFloat = 100
+    private let imageHeight: CGFloat = 100
+    
     var body: some View {
         VStack {
-            AsyncImage(url: viewModel.imageURL)
-                .frame(maxHeight: .infinity)
-          
-            HStack{
-                    VStack(alignment: .leading) {
-                        Text(viewModel.trackName)
-                        Text(viewModel.artistName).bold()
-                        Text(viewModel.price)
-                        Text("")
-                        Text(viewModel.duration)
-                        Text(viewModel.releaseDate)
-                        Spacer()
-                    }
-                    .padding()
+            HStack(alignment: .center){
+                
+                AsyncImage(url: viewModel.imageURL)
+                    .frame(width: imageWidth, height: imageHeight)
+                
+            }
+            .frame(maxHeight: .infinity)
+            
+            HStack(alignment: .center) {
+                if (horizontalSizeClass != .compact)
+                {
+                    Spacer()
+                }
+                VStack(alignment: .leading) {
+                    Text(viewModel.trackName)
+                    Text(viewModel.artistName).bold()
+                    Text(viewModel.price)
+                    Text("")
+                    Text(viewModel.duration)
+                    Text(viewModel.releaseDate)
+                }
+                .padding()
                 Spacer()
             }
-            
-            
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             VStack {
                 Button {
-                   //will launch a webview with the trackview url
+                    //will launch a webview with the trackview url
                 }
                 label: {
                     Text("More Details")
