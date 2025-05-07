c75c1bcfda3c4d90627e4fb1bf2c901e44b49ac3
Calum Maclellan <calummaclellan1@gmail.com>
2025-05-07 10:49:54 +0100

    initial commit

---
+++ ScotPowTechTest/ScotPowTechTest/ScotPowTechTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScotPowTechTestApp.swift
+//  ScotPowTechTest
+//
+//  Created by Calum Maclellan on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScotPowTechTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
